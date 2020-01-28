9ff29dd167f482e085612e8a9964c4bcb5fdbe63
MichaelLambooy <50894525+MichaelLambooy@users.noreply.github.com>
2020-01-28 15:29:49 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import './App.css';
+import FormPage from './components/FormComponent';
+import Weather from './components/WeatherComponent';
+
+const Api_Key = "072d16aa989b726d20052b1cf6786f43";
+
+class App extends React.Component {
+
+  state = {
+
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  //getWeather is a method we'll use to make the api call
+  getWeather = async (e) => {
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();   
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}&units=imperial`);
+    const response = await api_call.json();
+    console.log(response);
+    if(city && country){
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    }else{
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
+  render() {
+    return (
+<div className="App-header">
+<div class="bird-container bird-container--one">
+		<div class="bird bird--one"></div>
+	</div>
+	
+	<div class="bird-container bird-container--two">
+		<div class="bird bird--two"></div>
+	</div>
+	
+	<div class="bird-container bird-container--three">
+		<div class="bird bird--three"></div>
+	</div>
+	
+	<div class="bird-container bird-container--four">
+		<div class="bird bird--four"></div>
+	</div>
+  
+<h1>Weather</h1>
+  <p>Search for Weather Conditions</p>
+  <FormPage loadWeather={this.getWeather} />
+          <Weather
+            temperature={this.state.temperature}
+            city={this.state.city}
+            country={this.state.country}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+</div>
+
+
+    )
+  }
+}
+export default App;
