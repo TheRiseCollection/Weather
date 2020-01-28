ad57be7dc8f36454149f91d10841ddde94f37144
joshua <joshua@dynepic.com>
2020-01-28 15:57:17 -0500

    form input fixed weather data displays

--- src/components /FormComponent.js
+++ src/components /FormComponent.js
@@ -3,6 +3,7 @@ import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCard, MDBCardBody, MDBIcon } f
 
 class FormPage extends React.Component {
   state = this.props.initialState || {}
+  
 render() {
   return (
     <MDBContainer>
@@ -10,38 +11,33 @@ render() {
         <MDBCol md="6">
           <MDBCard>
             <MDBCardBody>
-              <form >
-                <p className="h4 text-center py-4">Search Weather</p>
+
+            <form onSubmit = {this.props.loadWeather}>
+            <p className="h4 text-center py-4">Search Weather</p>
                 <label
-                  htmlFor="defaultFormCardNameEx"
                   className="grey-text font-weight-light"
                 >
                   City
                 </label>
-                <input
-                  type="text"
-                  id="defaultFormCardNameEx"
-                  className="form-control"
-                />
+                <input type="text" name="city" placeholder="City..."/>
                 <br />
                 <label
-                  htmlFor="defaultFormCardEmailEx"
                   className="grey-text font-weight-light"
                 >
                   Country
                 </label>
-                <input
-                  type="text"
-                  id="defaultFormCardEmailEx"
-                  className="form-control"
-                />
-                <div className="text-center py-4 mt-3">
-                  <MDBBtn className="btn btn-outline-purple" type="submit" onSubmit={this.props.loadWeather}>
+                <input type="text" name="country" placeholder="Country..."/>
+
+      <div className="text-center py-4 mt-3">
+                  <MDBBtn className="btn btn-outline-purple" type="submit" >
                     Send
                     <MDBIcon far icon="paper-plane" className="ml-2" />
                   </MDBBtn>
                 </div>
-              </form>
+  </form>
+
+
+            
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
